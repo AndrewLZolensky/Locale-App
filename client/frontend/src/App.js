f61261cb46bb23e3721ece996138386d10e6ff85
Andrew Zolensky <andrewlzolensky@gmail.com>
2024-01-26 04:35:59 -0500

    added sign-in page

--- client/frontend/src/App.js
+++ client/frontend/src/App.js
@@ -1,9 +1,24 @@
+import axios from 'axios';
+import './App.css';
+import SignIn from './SignIn.js'
 
+const apiCall = () => {
+  axios.get('http://localhost:8080').then((data) => {
+    console.log(data);
+    })
+}
 
+/*
+    <div className="App">
+      <header className="App-header">
+        <button onClick={apiCall}>Make Api Call</button>
+      </header>
+    </div>
+*/
 function App() {
   return (
-    <div>
-      Hello
+    <div className="App">
+      <SignIn/>
     </div>
   );
 }
